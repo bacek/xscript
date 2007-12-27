bb6f93b1d3c52ded5819b39b28dd3e1c54456e2f
golubtsov <golubtsov@b01ef89b-65f2-463d-9415-e8412542ae63>
2007-12-27 08:57:01 +0000

    Startup scripts changed
    git-svn-id: https://xscript.opensource.yandex.ru/xscript/trunk@21 b01ef89b-65f2-463d-9415-e8412542ae63

--- extra/xscriptstart.sh
+++ extra/xscriptstart.sh
@@ -3,29 +3,34 @@
 
 #Main options
 ETCDIR='/etc'
-THIS=`basename $0 | sed 's/^[SK][0123456789]*b/b/'`
-XSCRIPT_CONFIG=$1
-LOCKFILE=$2/xscriptstart.pid
-LOGFILE=$3/xscriptstart.log
+NAME=$1
+WORK_DIR=$ETCDIR/$NAME
+
+XSCRIPT_CONFIG=$WORK_DIR/xscript.conf
+CUSTOM_FILE_TEMPLATE=xscript-*\.conf
+
+LOCKFILE="/var/run/$NAME/xscriptstart.pid"
+LOGFILE="/var/log/$NAME/xscriptstart.log"
 RDELAY=1
 
-#Ulimit option
-COREDIR=/var/tmp/core-files
-ulimit -s 1024
-ulimit -c unlimited
-
+for CUSTOM_CONFIG in $WORK_DIR/$CUSTOM_FILE_TEMPLATE
+do
+    if [ -r $CUSTOM_CONFIG ]; then
+	. $CUSTOM_CONFIG
+    fi
+done
 
 if [ -e $XSCRIPT_CONFIG  ]; then
     touch $LOCKFILE
-    trap "rm $LOCFILE 2>/dev/null" 0
-    ps ax | grep "xscriptstart.sh" | grep -v grep | awk '{print $1}' > $LOCKFILE
+    trap "rm $LOCKFILE 2>/dev/null" 0
+    ps ax | grep "xscriptstart.sh $NAME$" | grep -v grep | awk '{print $1}' > $LOCKFILE
     if [ -d "$COREDIR" ]; then
 	mkdir -p $COREDIR 2>/dev/null
     fi
     while [ -f $LOCKFILE ]; do
 	echo "/usr/bin/xscript-bin --config=$XSCRIPT_CONFIG"
         /usr/bin/xscript-bin --config=$XSCRIPT_CONFIG >> $LOGFILE 2>&1
-        echo "on `date` ::$THIS:: $THIS on `hostname` restarted"  >>$LOGFILE
+        echo "on `date` ::xscriptstart.sh:: xscriptstart.sh on `hostname` restarted"  >>$LOGFILE
         sleep $RDELAY
     done
 else
