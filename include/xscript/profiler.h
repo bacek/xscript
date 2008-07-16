35e4377e13edba199fe241b9fba8df8c210f3246
bacek <bacek@b01ef89b-65f2-463d-9415-e8412542ae63>
2008-07-16 12:27:52 +0000

    Factor operator-(endTime, startTime).
    git-svn-id: https://xscript.opensource.yandex.ru/xscript/trunk@327 b01ef89b-65f2-463d-9415-e8412542ae63

--- include/xscript/profiler.h
+++ include/xscript/profiler.h
@@ -7,6 +7,18 @@
 namespace xscript
 {
 
+    /**
+     * Calculate timeval delta in milliseconds
+     */
+    uint64_t operator-(const timeval& endTime, const timeval &startTime) {
+		return (endTime.tv_sec - startTime.tv_sec) * 1000000 
+            + endTime.tv_usec - startTime.tv_usec;
+    };
+
+    /**
+     * Profile some function.
+     * Wraps function end returns pair of function res and passed time.
+     */
 	template<typename Ret>
 	std::pair<Ret, uint64_t> profile(const boost::function<Ret ()>& f) {
 		struct timeval startTime, endTime;
@@ -15,11 +27,13 @@ namespace xscript
 		Ret res = f();
 
 		gettimeofday(&endTime, 0);
-		uint64_t delta = (endTime.tv_sec - startTime.tv_sec) * 1000000 + endTime.tv_usec - startTime.tv_usec;
-
-		return std::make_pair(res, delta);
+		return std::make_pair(res, endTime - startTime);
 	}
 
+    /**
+     * Specialisation of profile for functions returning void.
+     * Return just passed time.
+     */
 	uint64_t profile(const boost::function<void ()>& f) {
 		struct timeval startTime, endTime;
 		gettimeofday(&startTime, 0);
@@ -27,9 +41,8 @@ namespace xscript
 		f();
 
 		gettimeofday(&endTime, 0);
-		uint64_t delta = (endTime.tv_sec - startTime.tv_sec) * 1000000 + endTime.tv_usec - startTime.tv_usec;
 
-		return delta;
+		return endTime - startTime;
 	}
 
 }
