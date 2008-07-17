84c155d1b5ca9b3d1066efe29d02df38ac4159cd
golubtsov <golubtsov@b01ef89b-65f2-463d-9415-e8412542ae63>
2008-07-17 11:17:37 +0000

    Xml::fullName fixed
    git-svn-id: https://xscript.opensource.yandex.ru/xscript/trunk@337 b01ef89b-65f2-463d-9415-e8412542ae63

--- library/xml.cpp
+++ library/xml.cpp
@@ -1,7 +1,6 @@
 #include "settings.h"
 
 #include <boost/filesystem/path.hpp>
-#include <libxml/uri.h>
 
 #include "xscript/xml.h"
 #include "xscript/xml_helpers.h"
@@ -25,36 +24,35 @@ Xml::~Xml() {
 std::string
 Xml::fullName(const std::string &object) const {
 
-        if (object.empty()) {
-                throw std::runtime_error("Empty relative path");
-        }
+	if (object.empty()) {
+		throw std::runtime_error("Empty relative path");
+	}
 
-        fs::path path;
-        if (*object.begin() == '/') {
-                path = fs::path(object);
-        }
-        else {
-                const std::string &owner_name = name();
-                if (owner_name.empty()) {
-                        path = fs::path(object);
-                }
-                else if (*owner_name.rbegin() == '/') {
-                        path = fs::path(owner_name + object);
-                }
-                else {
-                        std::string::size_type pos = owner_name.find_last_of('/');
-                        if (pos == std::string::npos) {
-                                path = fs::path(object);
-                        }
-                        else {
-                                path = fs::path(owner_name.substr(0, pos + 1) + object);
-                        }
-                }
-        }
+	fs::path path;
+	if (*object.begin() == '/') {
+		path = fs::path(object);
+	}
+	else {
+		const std::string &owner_name = name();
+		if (owner_name.empty()) {
+			path = fs::path(object);
+		}
+		else if (*owner_name.rbegin() == '/') {
+			path = fs::path(owner_name + object);
+		}
+		else {
+			std::string::size_type pos = owner_name.find_last_of('/');
+			if (pos == std::string::npos) {
+				path = fs::path(object);
+			}
+			else {
+				path = fs::path(owner_name.substr(0, pos + 1) + object);
+			}
+		}
+	}
 
-        XmlCharHelper canonic_path(xmlCanonicPath((const xmlChar *) path.native_file_string().c_str()));
-
-        return (const char*) canonic_path.get();
+	path.normalize();
+	return path.string();
 }
 
 } // namespace xscript
