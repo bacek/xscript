22a0139927778410d4c7c5c647ac94fcceba4640
Vasily Chekalkin <bacek@yandex-team.ru>
2008-08-03 14:44:59 +1000

    Implementation for CacheCounter

--- standard/cache_counter_impl.cpp
+++ standard/cache_counter_impl.cpp
@@ -1,15 +1,42 @@
 #include <boost/lexical_cast.hpp>
-#include "xscript/cache_counter.h"
+#include "cache_counter_impl.h"
 
 namespace xscript
 {
 
-CacheCounter::CacheCounter(const std::string& name)
-	: name_(name), usedMemory_(0), stored_(0), loaded_(0), removed_(0)
+CacheCounterImpl::CacheCounterImpl(const std::string& name)
+	: CounterImpl(name), usedMemory_(0), stored_(0), loaded_(0), removed_(0)
 {
 }
 
-XmlNodeHelper CacheCounter::createReport() const {
+// FIXME We should use atomic_increment in this methods
+void CacheCounterImpl::incUsedMemory(size_t amount) { 
+    boost::mutex::scoped_lock l(mtx_);
+    usedMemory_ += amount;
+}
+void CacheCounterImpl::decUsedMemory(size_t amount) {
+    boost::mutex::scoped_lock l(mtx_);
+    usedMemory_ -= amount;
+}
+
+void CacheCounterImpl::incLoaded() {
+    boost::mutex::scoped_lock l(mtx_);
+    ++loaded_;
+}
+
+void CacheCounterImpl::incStored() {
+    boost::mutex::scoped_lock l(mtx_);
+    ++stored_;
+}
+
+void CacheCounterImpl::incRemoved() {
+    boost::mutex::scoped_lock l(mtx_);
+    ++removed_;
+}
+
+XmlNodeHelper CacheCounterImpl::createReport() const {
+    boost::mutex::scoped_lock l(mtx_);
+
 	XmlNodeHelper line(xmlNewNode(0, BAD_CAST name_.c_str()));
 
 	xmlSetProp(line.get(), BAD_CAST "used-memory", BAD_CAST boost::lexical_cast<std::string>(usedMemory_).c_str());
@@ -19,5 +46,20 @@ XmlNodeHelper CacheCounter::createReport() const {
 
 	return line;
 }
+
+
+
+class CacheCounterFactoryImpl : public CacheCounterFactory {
+public:
+    virtual std::auto_ptr<CacheCounter> createCounter(const std::string& name);
+};
+
+std::auto_ptr<CacheCounter> 
+CacheCounterFactoryImpl::createCounter(const std::string &name) {
+    return std::auto_ptr<CacheCounter>(new CacheCounterImpl(name));
+}
+
+REGISTER_COMPONENT2(CacheCounterFactory, CacheCounterFactoryImpl);
+
 
 }
--- standard/cache_counter_impl.h
+++ standard/cache_counter_impl.h
@@ -1,8 +1,9 @@
-#ifndef _XSCRIPT_CACHE_COUNTER_
-#define _XSCRIPT_CACHE_COUNTER_
+#ifndef _XSCRIPT_CACHE_COUNTER_IMPL_H
+#define _XSCRIPT_CACHE_COUNTER_IMPL_H
 
 #include <string>
-#include <xscript/counter_base.h>
+#include <xscript/cache_counter.h>
+#include "counter_impl.h"
 
 namespace xscript
 {
@@ -11,21 +12,20 @@ namespace xscript
 	 * Counter for measure cache statistic.
 	 * Trivial set of (loaded, stored, removed) counters.
 	 */
-	class CacheCounter : public CounterBase
+	class CacheCounterImpl : public CacheCounter, private CounterImpl
 	{
 	public:
-		CacheCounter(const std::string& name);
+		CacheCounterImpl(const std::string& name);
 		virtual XmlNodeHelper createReport() const;
 
-		void incUsedMemory(size_t amount) { usedMemory_ += amount; }
-		void decUsedMemory(size_t amount) { usedMemory_ -= amount; }
+		void incUsedMemory(size_t amount);
+		void decUsedMemory(size_t amount);
 
-		void incLoaded() { ++loaded_; }
-		void incStored() { ++stored_; }
-		void incRemoved() { ++removed_; }
+		void incLoaded();
+		void incStored();
+		void incRemoved();
 
 	private:
-        std::string name_;
 		size_t usedMemory_;
 		size_t stored_;
 		size_t loaded_;
