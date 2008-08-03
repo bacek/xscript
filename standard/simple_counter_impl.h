4eec56efe6d87bb89e02594d46793dffdab21b98
bacek <bacek@b01ef89b-65f2-463d-9415-e8412542ae63>
2008-08-03 06:23:49 +0000

    Remove commented out code
    git-svn-id: https://xscript.opensource.yandex.ru/xscript/trunk@383 b01ef89b-65f2-463d-9415-e8412542ae63

--- standard/simple_counter_impl.h
+++ standard/simple_counter_impl.h
@@ -23,20 +23,6 @@ namespace xscript
 		uint64_t peak_;
 	};
 
-    /*
-    class SimpleCounterFactoryImpl : public Component<SimpleCounterFactory>
-    {
-    public:
-        SimpleCounterFactoryImpl();
-        ~SimpleCounterFactoryImpl();
-
-        friend class ComponentRegisterer<SimpleCounterFactory>;
-
-        virtual void init(const Config *config);
-
-        std::auto_ptr<SimpleCounter> createCounter(const std::string& name);
-    };
-    */
 }
 
 #endif
