63e4b05442c7406bedf98840ccf8886cddf78c7a
Vasily Chekalkin <bacek@yandex-team.ru>
2008-08-23 00:21:45 +1000

    First cut of full-page-cache implementation. Not tested at all. But at least it compiles.

--- library/server.cpp
+++ library/server.cpp
@@ -35,6 +35,8 @@
 #include "xscript/vhost_data.h"
 #include "xscript/checking_policy.h"
 #include "xscript/xslt_profiler.h"
+#include "xscript/tag.h"
+#include "xscript/doc_cache.h"
 
 #ifdef HAVE_DMALLOC_H
 #include <dmalloc.h>
@@ -44,6 +46,7 @@ namespace xscript {
 
 extern "C" int closeFunc(void *ctx);
 extern "C" int writeFunc(void *ctx, const char *data, int len);
+extern "C" int writeBufFunc(void *ctx, const char *data, int len);
 
 Server::Server(Config *config) :
         config_(config) {
@@ -102,7 +105,92 @@ Server::handleRequest(RequestData *request_data) {
         }
 
         if (script->forceStylesheet() && needApplyStylesheet(ctx->request())) {
-            script->applyStylesheet(ctx.get(), *doc);
+            // Handle full page cache.
+            // We have to store headers and body.
+            // So, at the moment, create new trivial doc with full
+            // serialized response.
+            // TODO It's ugly hack to prove concept. Create big response document
+            // and store it in cache
+            if (script->tagged()) {
+                if (res.cached) {
+                    XmlDocHelper loaded_doc;
+                    Tag tag;
+                    bool l = DocCache::instance()->loadDoc(ctx.get(), script.get(), tag, loaded_doc);
+                    assert(l);
+
+                    // Iterate over children of root node. It should be (header*, content);
+                    xmlNodePtr root = xmlDocGetRootElement(loaded_doc.get());
+
+                    xmlNodePtr node = root->children;
+                    while (node) {
+                        if (strcmp((const char*) node->name, "header") == 0) {
+                            ctx->response()->setHeader((const char*)node->name, XmlUtils::value(node));
+                        }
+                        else if (strcmp((const char*) node->name, "content") == 0) {
+                            sendHeaders(ctx.get());
+                        }
+                        else {
+                            // Sounds like broken doc
+                            assert(!node->name);
+                            std::string out = XmlUtils::value(node);
+                            ctx->response()->write(out.c_str(), out.length());
+                            return;
+                        }
+
+                        node = node->next;
+                    }
+
+                }
+                else {
+                    // Store output headers and body for future use
+                    XmlDocHelper new_doc(xmlNewDoc((const xmlChar*) "1.0"));
+                    xmlNodePtr root = xmlNewDocNode(new_doc.get(), NULL, BAD_CAST "result", 0);
+                    XmlUtils::throwUnless(NULL != root);
+
+                    xmlDocSetRootElement(new_doc.get(), root);
+
+                    // Store headers
+                    Response::Headers headers = ctx->response()->getHeaders();
+                    for(Response::Headers::iterator h = headers.begin(); h != headers.end(); ++h) {
+                        XmlNodeHelper header(xmlNewNode(0, BAD_CAST "header"));
+                        xmlSetProp(header.get(), BAD_CAST "name", BAD_CAST h->first.c_str());
+                        xmlSetProp(header.get(), BAD_CAST "value", BAD_CAST h->second.c_str());
+                        xmlAddChild(root, header.get());
+                        header.release();
+                    }
+
+                    std::string out;
+                    
+                    xmlCharEncodingHandlerPtr encoder = NULL;
+                    const std::string &encoding = ctx->documentWriter()->outputEncoding();
+                    if (!encoding.empty()) {
+                        encoder = xmlFindCharEncodingHandler(encoding.c_str());
+                    }
+                    // If there is no errors libxml will close buffer.
+                    buf = xmlOutputBufferCreateIO(&writeBufFunc, &closeFunc, &out, encoder);
+                    XmlUtils::throwUnless(NULL != buf);
+                    ctx->documentWriter()->write(ctx->response(), *doc, buf);
+
+                    // Create single text node for response
+                    XmlNodeHelper content(xmlNewNode(0, BAD_CAST "content"));
+                    xmlAddChild(content.get(), xmlNewTextLen(BAD_CAST out.c_str(), out.length()));
+                    xmlAddChild(root, content.get());
+                    content.release();
+
+                    // And finally store doc in cache
+                    Tag tag;
+                    DocCache::instance()->saveDoc(ctx.get(), script.get(), tag, new_doc);
+
+                    // Now. Send all data to user. He waited enought.
+                    sendHeaders(ctx.get());
+                    ctx->response()->write(out.c_str(), out.length());
+                }
+
+                return;
+            }
+            else {
+                script->applyStylesheet(ctx.get(), *doc);
+            }
         }
         else {
             script->removeUnusedNodes(*res.doc.get());
@@ -188,6 +276,16 @@ extern "C" int
                      context->request()->getScriptFilename().c_str(), e.what());
     }
     return -1;
+}
+
+// Write result of serialisation from libxml to buffer
+extern "C" int
+writeBufFunc(void *ctx, const char *data, int len) {
+    if(len == 0) 
+        return 0;
+    std::string * str = static_cast<std::string*>(ctx);
+    str->append(data, len);
+    return len;
 }
 
 } // namespace xscript
