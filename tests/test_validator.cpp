c9f66cd532ee38b7a88dc0136ec228487e52bb44
Vasily Chekalkin <bacek@yandex-team.ru>
2008-09-22 22:13:23 +1000

    WIP

--- tests/test_validator.cpp
+++ tests/test_validator.cpp
@@ -2,17 +2,26 @@
 #include <cppunit/extensions/HelperMacros.h>
 
 #include "xscript/validator_factory.h"
+#include "xscript/xml_helpers.h"
 
 using namespace xscript;
 
 class ValidatorTest : public CppUnit::TestFixture {
     CPPUNIT_TEST_SUITE(ValidatorTest);
-    CPPUNIT_TEST(testCreate);
+    CPPUNIT_TEST(testCreateFactory);
+    CPPUNIT_TEST(testCreateNonexisted);
     CPPUNIT_TEST_SUITE_END();
 
-    void testCreate() {
+    void testCreateFactory() {
         ValidatorFactory * factory = ValidatorFactory::instance();
         CPPUNIT_ASSERT(factory);
+    }
+
+    void testCreateNonexisted() {
+        ValidatorFactory * factory = ValidatorFactory::instance();
+
+        XmlNodeHelper node(xmlNewNode(NULL, BAD_CAST "param"));
+        std::auto_ptr<ValidatorBase> val = factory->createValidator(node.get());
     }
 };
 
