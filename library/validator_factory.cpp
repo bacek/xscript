b96b41d0504c761461d0d6139087f5b62043a137
Vasily Chekalkin <bacek@yandex-team.ru>
2008-09-22 21:59:45 +1000

    Stub for ValidatorFactory

---
+++ library/validator_factory.cpp
@@ -0,0 +1,15 @@
+#include "xscript/validator_factory.h"
+
+namespace xscript
+{
+
+REGISTER_COMPONENT(ValidatorFactory);
+
+ValidatorFactory::ValidatorFactory() {
+}
+
+ValidatorFactory::~ValidatorFactory() {
+}
+
+
+}
