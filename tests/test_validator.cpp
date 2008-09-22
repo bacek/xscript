8f4a1df0523e1c3da6de8cc9559488552fdd4fe8
Vasily Chekalkin <bacek@yandex-team.ru>
2008-09-22 22:00:11 +1000

    First test: ValidatorFactory created

--- tests/test_validator.cpp
+++ tests/test_validator.cpp
@@ -1,11 +1,21 @@
 #include <cppunit/TestFixture.h>
 #include <cppunit/extensions/HelperMacros.h>
 
-class ValidatorTest : public CppUnit::TestFixture {
+#include "xscript/validator_factory.h"
 
+using namespace xscript;
+
+class ValidatorTest : public CppUnit::TestFixture {
     CPPUNIT_TEST_SUITE(ValidatorTest);
+    CPPUNIT_TEST(testCreate);
     CPPUNIT_TEST_SUITE_END();
+
+    void testCreate() {
+        ValidatorFactory * factory = ValidatorFactory::instance();
+        CPPUNIT_ASSERT(factory);
+    }
 };
 
 CPPUNIT_TEST_SUITE_NAMED_REGISTRATION(ValidatorTest, "validator");
 CPPUNIT_REGISTRY_ADD("validator", "xscript");
+
