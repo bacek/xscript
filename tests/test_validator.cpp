6f9e13c17b7ce729f1f751a4fd726208109ecf03
Vasily Chekalkin <bacek@yandex-team.ru>
2008-09-23 21:30:38 +1000

    More tests

--- tests/test_validator.cpp
+++ tests/test_validator.cpp
@@ -6,10 +6,23 @@
 
 using namespace xscript;
 
+class ValidatorMockup : public xscript::ValidatorBase {
+public:
+    ValidatorMockup(xmlNodePtr node) {}
+};
+
+ValidatorBase * createMockup(xmlNodePtr node) {
+    return new ValidatorMockup(node);
+}
+
 class ValidatorTest : public CppUnit::TestFixture {
     CPPUNIT_TEST_SUITE(ValidatorTest);
     CPPUNIT_TEST(testCreateFactory);
-    CPPUNIT_TEST(testCreateNonexisted);
+    CPPUNIT_TEST(testCreateNoop);
+    CPPUNIT_TEST_EXCEPTION(testCreateNonexisted, std::runtime_error);
+    CPPUNIT_TEST(testRegisterContructor);
+    CPPUNIT_TEST_EXCEPTION(testRegisterContructor, std::runtime_error);
+    CPPUNIT_TEST(testConstruct);
     CPPUNIT_TEST_SUITE_END();
 
     void testCreateFactory() {
@@ -17,12 +30,43 @@ class ValidatorTest : public CppUnit::TestFixture {
         CPPUNIT_ASSERT(factory);
     }
 
-    void testCreateNonexisted() {
+    void testCreateNoop() {
         ValidatorFactory * factory = ValidatorFactory::instance();
 
         XmlNodeHelper node(xmlNewNode(NULL, BAD_CAST "param"));
         std::auto_ptr<ValidatorBase> val = factory->createValidator(node.get());
+        CPPUNIT_ASSERT(!val.get());
     }
+    
+    void testCreateNonexisted() {
+        ValidatorFactory * factory = ValidatorFactory::instance();
+
+        XmlNodeHelper node(xmlNewNode(NULL, BAD_CAST "param"));
+        xmlNewProp(node.get(), BAD_CAST "validator", BAD_CAST "foo");
+        std::auto_ptr<ValidatorBase> val = factory->createValidator(node.get());
+    }
+
+    // We use this function twice. On second call it should throw runtime_error
+    void testRegisterContructor() {
+        ValidatorFactory * factory = ValidatorFactory::instance();
+        factory->registerConstructor("foo", &createMockup);
+    }
+
+    void testConstruct() {
+        ValidatorFactory * factory = ValidatorFactory::instance();
+        // We don't have to register constructor. It's already register by
+        // previous test
+
+        XmlNodeHelper node(xmlNewNode(NULL, BAD_CAST "param"));
+        xmlNewProp(node.get(), BAD_CAST "validator", BAD_CAST "foo");
+        std::auto_ptr<ValidatorBase> val = factory->createValidator(node.get());
+        CPPUNIT_ASSERT(val.get());
+
+        // Check that we've got our mockup.
+        ValidatorMockup * real = dynamic_cast<ValidatorMockup*>(val.get());
+        CPPUNIT_ASSERT(real);
+    }
+    
 };
 
 CPPUNIT_TEST_SUITE_NAMED_REGISTRATION(ValidatorTest, "validator");
