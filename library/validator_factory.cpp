7fecccbdbfac62e984abdbeb09b8b341e4785328
Vasily Chekalkin <bacek@yandex-team.ru>
2008-09-23 19:57:08 +1000

    Implement creation of validators.

--- library/validator_factory.cpp
+++ library/validator_factory.cpp
@@ -1,4 +1,5 @@
 #include "xscript/validator_factory.h"
+#include "xscript/xml_util.h"
 
 namespace xscript
 {
@@ -13,7 +14,26 @@ ValidatorFactory::~ValidatorFactory() {
 
 std::auto_ptr<ValidatorBase>
 ValidatorFactory::createValidator(xmlNodePtr node) {
-    return std::auto_ptr<ValidatorBase>();
+    std::string type;
+    xmlAttrPtr attr = node->properties;
+    while (attr) {
+        const char *name = (const char*) attr->name;
+        if (strcmp("validator", name) == 0) {
+            type = XmlUtils::value(attr);
+            break;
+        }
+        attr = attr->next;
+    }
+
+    // If validator absent for param return null.
+    if (type.empty())
+        return std::auto_ptr<ValidatorBase>();
+
+    ValidatorMap::const_iterator i = validator_creators_.find(type);
+    if (i == validator_creators_.end())
+        throw std::runtime_error("Unknown validator type: " + type);
+
+    return std::auto_ptr<ValidatorBase>((*i->second)(node));
 }
 
 }
