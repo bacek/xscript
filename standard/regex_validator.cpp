6b12ad58978958847fc9f97f883097a9da0331f6
Vasily Chekalkin <bacek@yandex-team.ru>
2008-10-10 09:30:42 +1100

    First working implementation of RegexValidator

--- standard/regex_validator.cpp
+++ standard/regex_validator.cpp
@@ -1,5 +1,7 @@
+#include <iostream>
 #include "regex_validator.h"
 #include "xscript/param.h"
+#include "xscript/xml_util.h"
 
 namespace xscript
 {
@@ -7,9 +9,24 @@ namespace xscript
 RegexValidator::RegexValidator(xmlNodePtr node)
     : Validator(node), re_(NULL)
 {
-    xmlAttrPtr pattern = xmlHasProp(node, (const xmlChar*)"pattern");
-    if (!pattern)
+    xmlAttrPtr pattern_attr = xmlHasProp(node, (const xmlChar*)"pattern");
+    if (!pattern_attr)
         throw std::runtime_error("Pattern not provided");
+  
+
+    std::string pattern = XmlUtils::value(pattern_attr);
+    xmlRemoveProp(pattern_attr); // libxml will free memory
+
+    int pcre_options = 0;
+    const char* compile_error;
+    int error_offset;
+    re_ = pcre_compile(pattern.c_str(), pcre_options,
+                      &compile_error, &error_offset, NULL);
+  
+    if (re_ == NULL)
+        throw std::runtime_error(std::string("Regex compilation failed: ") + compile_error);
+    
+    bzero((void*)&re_extra_, sizeof(re_extra_));
 }
 
 RegexValidator::~RegexValidator()
@@ -21,7 +38,15 @@ RegexValidator::~RegexValidator()
 bool 
 RegexValidator::isPassed(const Context *ctx, const Param &value) const
 {
-    return true;
+    return checkString(value.asString(ctx));
+}
+
+bool 
+RegexValidator::checkString(const std::string &value) const
+{
+    int rc = pcre_exec(re_, &re_extra_, value.c_str(), value.length(), 0, 0, NULL, 0);
+//    std::cerr << "rc: " << rc << std::endl;
+    return rc != -1;
 }
 
 Validator* RegexValidator::create(xmlNodePtr node) {
