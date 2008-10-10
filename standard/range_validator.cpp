cf73a274bc1a27aadbb336f0b987e10e97cbe5e5
Vasily Chekalkin <bacek@yandex-team.ru>
2008-10-10 21:39:53 +1100

    Split RangeValidator into header and implementation

--- standard/range_validator.cpp
+++ standard/range_validator.cpp
@@ -1,66 +1,7 @@
-#include <boost/lexical_cast.hpp>
-#include "xscript/validator.h"
-#include "xscript/param.h"
-#include "xscript/xml_util.h"
+#include "range_validator.h"
 
 namespace xscript
 {
-
-/**
- * Range validator. Validate that numeric value of param within given range.
- * Example of usage:
- *   param type="QueryArg" id="foo" validator="int_range" min="1" max="42"
- *
- * At least min or max should be specified.
- *
- * TODO: We really need unit tests...
- */
-template<typename T>
-class RangeValidator : public Validator {
-public:
-    RangeValidator(xmlNodePtr node)
-	: Validator(node), has_min_(false), has_max_(false) {
-        xmlAttrPtr min = xmlHasProp(node, (const xmlChar*)"min");
-        if (min) {
-            has_min_ = true;
-            min_ = boost::lexical_cast<T>(XmlUtils::value(min));
-            xmlRemoveProp(min); // libxml will free memory
-        }
-
-        xmlAttrPtr max = xmlHasProp(node, (const xmlChar*)"max");
-        if (max) {
-            has_max_ = true;
-            max_ = boost::lexical_cast<T>(XmlUtils::value(max));
-            xmlRemoveProp(max); // libxml will free memory
-        }
-
-        if (!(has_min_ || has_max_)) {
-            throw std::runtime_error("Insufficient args for range validator");
-        }
-    }
-    ~RangeValidator() {}
-
-    static Validator* create(xmlNodePtr node) {
-        return (Validator*)new RangeValidator(node);
-    }
-
-protected:
-    /// Check. Return true if validator passed.
-    virtual bool isPassed(const Context *ctx, const Param &value) const {
-        try {
-            T val = boost::lexical_cast<T>(value.asString(ctx));
-            return (!has_min_ || (min_ <= val)) && (!has_max_ || (val <= max_));
-        }
-        catch(...) {
-            return false;
-        }
-    }
-
-private:
-    bool has_min_, has_max_;
-    T min_, max_;
-};
-
 
 static ValidatorRegisterer r1("int_range", &RangeValidator<int>::create);
 static ValidatorRegisterer r2("long_range", &RangeValidator<long>::create);
