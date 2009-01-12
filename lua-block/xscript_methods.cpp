db2933fba8b20d823e608548c17f9279483f1248
golubtsov <golubtsov@b01ef89b-65f2-463d-9415-e8412542ae63>
2009-01-12 14:27:04 +0000

    exception catching added to luaPrint
    git-svn-id: https://svn.opensource.yandex.net/xscript/trunk@687 b01ef89b-65f2-463d-9415-e8412542ae63

--- lua-block/xscript_methods.cpp
+++ lua-block/xscript_methods.cpp
@@ -21,38 +21,44 @@
 namespace xscript {
 
 static int luaPrint (lua_State *lua) {
-    int n = lua_gettop(lua);  /* number of arguments */
-    int i;
-
-    log()->debug("%s, stack size is: %d", BOOST_CURRENT_FUNCTION, lua_gettop(lua));
-    lua_getglobal(lua, "xscript");
-    lua_getfield(lua, -1, "_buf");
-
-    pointer<std::string> * p = (pointer<std::string>*)lua_touserdata(lua, -1);
-    assert(p);
-    std::string* buf = p->ptr;
-    assert(buf);
-
-    lua_pop(lua, 2); // pop xscript and buf
-    log()->debug("%s, stack size is: %d", BOOST_CURRENT_FUNCTION, lua_gettop(lua));
-
-    //std::string* buf = output_buffer_.get();
-
-    lua_getglobal(lua, "tostring");
-    for (i=1; i<=n; i++) {
-        const char *s;
-        lua_pushvalue(lua, -1);  /* function to be called */
-        lua_pushvalue(lua, i);   /* value to print */
-        lua_call(lua, 1, 1);
-        s = lua_tostring(lua, -1);  /* get result */
-        if (s == NULL)
-            return luaL_error(lua, LUA_QL("tostring") " must return a string to "
-                              LUA_QL("print"));
-        if (i>1) buf->append("\t");
-        buf->append(s);
-        lua_pop(lua, 1);  /* pop result */
+    try {
+        int n = lua_gettop(lua);  /* number of arguments */
+        int i;
+    
+        log()->debug("%s, stack size is: %d", BOOST_CURRENT_FUNCTION, lua_gettop(lua));
+        lua_getglobal(lua, "xscript");
+        lua_getfield(lua, -1, "_buf");
+    
+        pointer<std::string> * p = (pointer<std::string>*)lua_touserdata(lua, -1);
+        assert(p);
+        std::string* buf = p->ptr;
+        assert(buf);
+    
+        lua_pop(lua, 2); // pop xscript and buf
+        log()->debug("%s, stack size is: %d", BOOST_CURRENT_FUNCTION, lua_gettop(lua));
+    
+        //std::string* buf = output_buffer_.get();
+    
+        lua_getglobal(lua, "tostring");
+        for (i=1; i<=n; i++) {
+            const char *s;
+            lua_pushvalue(lua, -1);  /* function to be called */
+            lua_pushvalue(lua, i);   /* value to print */
+            lua_call(lua, 1, 1);
+            s = lua_tostring(lua, -1);  /* get result */
+            if (s == NULL)
+                return luaL_error(lua, LUA_QL("tostring") " must return a string to "
+                                  LUA_QL("print"));
+            if (i>1) buf->append("\t");
+            buf->append(s);
+            lua_pop(lua, 1);  /* pop result */
+        }
+        buf->append("\n");
     }
-    buf->append("\n");
+    catch (const std::exception &e) {
+        log()->error("caught exception in [xscript:print]: %s", e.what());
+        luaL_error(lua, e.what());
+    }
     return 0;
 }
 
@@ -109,7 +115,7 @@ luaUrlDecode(lua_State *lua) {
         return 1;
     }
     catch (const std::exception &e) {
-        log()->error("caught exception in [xscript:urlencode]: %s", e.what());
+        log()->error("caught exception in [xscript:urldecode]: %s", e.what());
         luaL_error(lua, e.what());
     }
     return 0;
@@ -127,7 +133,7 @@ luaMD5(lua_State *lua) {
         return 1;
     }
     catch (const std::exception &e) {
-        log()->error("caught exception in [xscript:urlencode]: %s", e.what());
+        log()->error("caught exception in [xscript:md5]: %s", e.what());
         luaL_error(lua, e.what());
     }
     return 0;
