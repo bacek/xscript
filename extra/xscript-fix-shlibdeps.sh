926c465333f6ecdc9cb4527c9284cdf2a9281e01
lmovsesjan <lmovsesjan@b01ef89b-65f2-463d-9415-e8412542ae63>
2009-01-22 08:36:24 +0000

    added xscript-fix-shlibdeps.sh script
    git-svn-id: https://svn.opensource.yandex.net/xscript/trunk@712 b01ef89b-65f2-463d-9415-e8412542ae63

---
+++ extra/xscript-fix-shlibdeps.sh
@@ -0,0 +1,32 @@
+# !/bin/sh
+
+CORE_PACKAGE="libxscript"
+PACKAGE=$1
+FILE="debian/$PACKAGE.substvars"
+
+if [ -f $PACKAGE ]; then
+    echo "Package name must be feeded as command line parameter"
+    exit 1
+fi
+
+if ! test -e $FILE; then
+    echo "File $FILE not found"
+    exit 1
+fi
+
+while read LINE
+do
+    MATCH=`echo $LINE | grep -c 'shlibs:Depends='`
+    if [ "1" = $MATCH ]; then
+        VERSION=`echo $LINE | sed -e 's/.*'$CORE_PACKAGE' (>=//' | sed -e 's/).*//'`
+        MAJOR_VERSION=`echo $VERSION | sed -e 's/[.].*//'`
+        MINOR_VERSION=`echo $VERSION | sed -e 's/.*[.]//'`
+        CORRECT_LINE=`echo -n $LINE, $CORE_PACKAGE '(<' $MAJOR_VERSION.$(($MINOR_VERSION+1))')'`
+        echo $CORRECT_LINE
+    else
+        echo $LINE
+    fi
+done < $FILE > $FILE.tmp
+
+mv $FILE.tmp $FILE
+
