a399ffd5d8d96632d588f58a4538bb02e5806062
golubtsov <golubtsov@b01ef89b-65f2-463d-9415-e8412542ae63>
2010-04-26 12:55:41 +0000

    version up
    git-svn-id: https://svn.yandex.ru/xscript-core/trunk@1273 b01ef89b-65f2-463d-9415-e8412542ae63

--- extra/xscript-fix-shlibdeps.sh
+++ extra/xscript-fix-shlibdeps.sh
@@ -20,7 +20,7 @@ do
     if [ "1" = $MATCH ]; then
         VERSION=`echo $LINE | sed -e 's/.*'$CORE_PACKAGE' (>=//' | sed -e 's/).*//'`
         MAJOR_VERSION=`echo $VERSION | sed -e 's/[.].*//'`
-        MINOR_VERSION=`echo $VERSION | sed -e 's/.*[.]//'`
+        MINOR_VERSION=`echo $VERSION | sed 's/.*\.\(.*\)\..*/\1/'`
         CORRECT_LINE=`echo -n $LINE, $CORE_PACKAGE '(<<' $MAJOR_VERSION.$(($MINOR_VERSION+1))')'`
         echo $CORRECT_LINE
     else
