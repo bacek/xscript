8eb14da8466cf5977d5020d53a3e687db6faafe6
ponomarev <ponomarev@b01ef89b-65f2-463d-9415-e8412542ae63>
2011-05-27 09:44:18 +0000

    Response::setCookie() override previous cookie with the same name
    git-svn-id: https://svn.yandex.ru/xscript-core/trunk@1522 b01ef89b-65f2-463d-9415-e8412542ae63

--- library/response.cpp
+++ library/response.cpp
@@ -28,6 +28,13 @@
 namespace xscript {
 
 static const boost::uint32_t DEFAULT_EXPIRE_TIME_DELTA = 300;
+static const std::string STR_STATUS("Status");
+static const std::string STR_CONTENT_TYPE("Content-Type");
+static const std::string STR_CONTENT_LENGTH("Content-Length");
+static const std::string STR_CONTENT_ENCODING("Content-Encoding");
+static const std::string STR_LOCATION("Location");
+static const std::string STR_REFERER("Referer");
+static const std::string STR_TEXT_HTML("text/html");
 
 class Response::ResponseData {
 public:
@@ -37,14 +44,25 @@ public:
     void sendHeaders();
     bool isBinary() const;
     bool cacheable() const;
-    
-    Response *response_;
 
+    void checkAllowedHeaders() const;
+
+    void setCookie(const Cookie &cookie);
+    void setHeaderValidName(const std::string &name, const std::string &value);
+    void setHeader(const std::string &name, const std::string &value);
+
+    void redirectToPath(const std::string &path, unsigned short status = 302);
+
+private:
+    void patchHeaderValidName(const std::string &name, const std::string &value);
+
+public:
+    Response *response_;
     std::ostream *stream_;
-    
+
     HeaderMap out_headers_;
     CookieSet out_cookies_;
-    
+
     std::auto_ptr<BinaryWriter> writer_;
 
     bool headers_sent_;
@@ -67,13 +85,20 @@ Response::ResponseData::~ResponseData()
 {}
 
 void
+Response::ResponseData::checkAllowedHeaders() const {
+    if (headers_sent_) {
+        throw std::runtime_error("headers already sent");
+    }
+}
+
+void
 Response::ResponseData::sendHeaders() {
     boost::mutex::scoped_lock sl(resp_mutex_);
     std::stringstream stream;
     if (!headers_sent_) {
         log()->debug("%s, sending headers", BOOST_CURRENT_FUNCTION);
         stream << status_ << " " << HttpUtils::statusToString(status_);
-        out_headers_["Status"] = stream.str();
+        out_headers_[STR_STATUS] = stream.str();
         response_->writeHeaders();
         headers_sent_ = true;
     }
@@ -89,6 +114,60 @@ Response::ResponseData::cacheable() const {
     return NULL != cache_data_.get();
 }
 
+void
+Response::ResponseData::setCookie(const Cookie &cookie) {
+    if (!cookie.check()) {
+        const Request *request = VirtualHostData::instance()->get();
+        std::string url = request ? request->getOriginalUrl() : StringUtils::EMPTY_STRING;
+        log()->warn("Incorrect cookie. Skipped. Url: %s Name: %s; value: %s; domain: %s; path: %s",
+                url.c_str(),
+                StringUtils::urlencode(cookie.name()).c_str(),
+                StringUtils::urlencode(cookie.value()).c_str(),
+                StringUtils::urlencode(cookie.domain()).c_str(),
+                StringUtils::urlencode(cookie.path()).c_str());
+        return;
+    }
+    boost::mutex::scoped_lock sl(resp_mutex_);
+    checkAllowedHeaders();
+    out_cookies_.erase(cookie);
+    out_cookies_.insert(cookie);
+}
+
+void
+Response::ResponseData::patchHeaderValidName(const std::string &name, const std::string &value) {
+    std::string::size_type pos = value.find_first_of("\r\n");
+    if (pos == std::string::npos) {
+        out_headers_[name].assign(value);
+    }
+    else {
+        out_headers_[name].assign(value.begin(), value.begin() + pos);
+    }
+}
+
+void
+Response::ResponseData::setHeaderValidName(const std::string &name, const std::string &value) {
+    boost::mutex::scoped_lock sl(resp_mutex_);
+    checkAllowedHeaders();
+    patchHeaderValidName(name, value);
+}
+
+void
+Response::ResponseData::setHeader(const std::string &name, const std::string &value) {
+    boost::mutex::scoped_lock sl(resp_mutex_);
+    checkAllowedHeaders();
+    std::string normalized_name = HttpUtils::normalizeOutputHeaderName(name);
+    patchHeaderValidName(normalized_name, value);
+}
+
+void
+Response::ResponseData::redirectToPath(const std::string &path, unsigned short status) {
+    boost::mutex::scoped_lock sl(resp_mutex_);
+    checkAllowedHeaders();
+    patchHeaderValidName(STR_LOCATION, path);
+    status_ = status;
+}
+
+
 Response::Response() : data_(new ResponseData(this, NULL)) {
 }
 
@@ -102,35 +181,14 @@ Response::~Response() {
 
 void
 Response::setCookie(const Cookie &cookie) {
-    if (!cookie.check()) {
-        const Request *request = VirtualHostData::instance()->get();
-        std::string url = request ? request->getOriginalUrl() : StringUtils::EMPTY_STRING;
-        log()->warn("Incorrect cookie. Skipped. Url: %s Name: %s; value: %s; domain: %s; path: %s",
-                url.c_str(),
-                StringUtils::urlencode(cookie.name()).c_str(),
-                StringUtils::urlencode(cookie.value()).c_str(),
-                StringUtils::urlencode(cookie.domain()).c_str(),
-                StringUtils::urlencode(cookie.path()).c_str());
-        return;
-    }
-    boost::mutex::scoped_lock sl(data_->resp_mutex_);
-    if (!data_->headers_sent_) {
-        data_->out_cookies_.insert(cookie);
-    }
-    else {
-        throw std::runtime_error("headers already sent");
-    }
+    data_->setCookie(cookie);
 }
 
 void
 Response::setStatus(unsigned short status) {
     boost::mutex::scoped_lock sl(data_->resp_mutex_);
-    if (!data_->headers_sent_) {
-        data_->status_ = status;
-    }
-    else {
-        throw std::runtime_error("headers already sent");
-    }
+    data_->checkAllowedHeaders();
+    data_->status_ = status;
 }
 
 void
@@ -138,15 +196,11 @@ Response::sendError(unsigned short status, const std::string &message) {
     log()->debug("%s, clearing request output", BOOST_CURRENT_FUNCTION);
     {
         boost::mutex::scoped_lock sl(data_->resp_mutex_);
-        if (!data_->headers_sent_) {
-            data_->out_cookies_.clear();
-            data_->out_headers_.clear();
-        }
-        else {
-            throw std::runtime_error("headers already sent");
-        }
+        data_->checkAllowedHeaders();
+        data_->out_cookies_.clear();
+        data_->out_headers_.clear();
         data_->status_ = status;
-        data_->out_headers_.insert(std::pair<std::string, std::string>("Content-type", "text/html"));
+        data_->out_headers_[STR_CONTENT_TYPE] = STR_TEXT_HTML;
     }
 
     boost::mutex::scoped_lock sl(data_->write_mutex_);
@@ -163,24 +217,13 @@ Response::sendError(unsigned short status, const std::string &message) {
 
 void
 Response::setHeader(const std::string &name, const std::string &value) {
-    boost::mutex::scoped_lock sl(data_->resp_mutex_);
-    if (data_->headers_sent_) {
-        throw std::runtime_error("headers already sent");
-    }
-    std::string normalized_name = HttpUtils::normalizeOutputHeaderName(name);
-    std::string::size_type pos = value.find_first_of("\r\n");
-    if (pos == std::string::npos) {
-        data_->out_headers_[normalized_name] = value;
-    }
-    else {
-        data_->out_headers_[normalized_name].assign(value.begin(), value.begin() + pos);
-    }
+    data_->setHeader(name, value);
 }
 
 void
 Response::setExpiresHeader() {
     boost::int32_t expires = HttpDateUtils::expires(expireDelta());
-    setHeader(HttpUtils::EXPIRES_HEADER_NAME, HttpDateUtils::format(expires));
+    data_->setHeaderValidName(HttpUtils::EXPIRES_HEADER_NAME, HttpDateUtils::format(expires));
 }
 
 void
@@ -254,7 +297,7 @@ Response::detach(Context *ctx) {
         if (data_->have_cached_copy_ && data_->cache_data_->notModified(ctx)) {
             setStatus(304);
             setExpiresHeader();
-            setHeader(HttpUtils::ETAG_HEADER_NAME, data_->cache_data_->etag());
+            data_->setHeaderValidName(HttpUtils::ETAG_HEADER_NAME, data_->cache_data_->etag());
             data_->cache_data_ = boost::shared_ptr<PageCacheData>(); //need for sending headers in non-cache mode
             data_->sendHeaders();
         }
@@ -263,7 +306,7 @@ Response::detach(Context *ctx) {
         }
     }
     else if (data_->isBinary()) {
-        setHeader("Content-length", boost::lexical_cast<std::string>(data_->writer_->size()));
+        data_->setHeaderValidName(STR_CONTENT_LENGTH, boost::lexical_cast<std::string>(data_->writer_->size()));
         data_->sendHeaders();
         writeByWriter(data_->writer_.get());
     }
@@ -293,9 +336,11 @@ Response::detach(Context *ctx) {
 
 bool
 Response::suppressBody(const Request *req) const {
+    if (req && req->suppressBody()) {
+        return true;
+    }
     boost::mutex::scoped_lock sl(data_->resp_mutex_);
-    return data_->suppress_body_ || req->suppressBody() ||
-        204 == data_->status_ || 304 == data_->status_;
+    return data_->suppress_body_ || 204 == data_->status_ || 304 == data_->status_;
 }
 
 void
@@ -380,23 +425,24 @@ Response::status() const {
 
 void
 Response::redirectBack(const Request *req) {
-    redirectToPath(req->getHeader("Referer"));
+    if (req) {
+        data_->redirectToPath(req->getHeader(STR_REFERER));
+    }
 }
 
 void
 Response::redirectToPath(const std::string &path) {
-    setStatus(302);
-    setHeader("Location", path);
+    data_->redirectToPath(path);
 }
 
 void
 Response::setContentType(const std::string &type) {
-    setHeader("Content-type", type);
+    data_->setHeaderValidName(STR_CONTENT_TYPE, type);
 }
 
 void
 Response::setContentEncoding(const std::string &encoding) {
-    setHeader("Content-encoding", encoding);
+    data_->setHeaderValidName(STR_CONTENT_ENCODING, encoding);
 }
 
 void
